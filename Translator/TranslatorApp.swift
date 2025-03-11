98858881b7fc8700c75e387166fea258f685b3d8
Hamza <muhammadhamzam1486@gmail.com>
2025-03-11 16:58:06 +0500

    Initial Commit

---
+++ Translator/TranslatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  TranslatorApp.swift
+//  Translator
+//
+//  Created by truID on 11/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct TranslatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
