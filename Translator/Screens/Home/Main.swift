0eec9fc7daf8e667d4da21a79940fc40f2d02ef3
Hamza <muhammadhamzam1486@gmail.com>
2025-03-11 20:07:38 +0500

    feat: create necessary boilerplate

---
+++ Translator/Screens/Home/Main.swift
@@ -0,0 +1,85 @@
+//
+//  ContentView.swift
+//  Translator
+//
+//  Created by truID on 11/03/2025.
+//
+
+import SwiftUI
+
+struct Main: View {
+    @State private var inputText: String = ""
+
+    @State private var lang1: String = "en"
+    @State private var lang2: String = "ur"
+
+    @ObservedObject private var vm = ViewModel()
+
+    var body: some View {
+        NavigationView {
+            VStack(alignment: .center) {
+                HStack(alignment: .center) {
+                    Text("Translate")
+                        .font(.title2)
+                }
+
+                Form {
+                    Section {
+                        Picker("From", selection: $lang1) {
+                            ForEach(languages, id: \.code) { (language: Language) in
+                                Text(language.name)
+                            }
+                        }
+                        Picker("To", selection: $lang2) {
+                            ForEach(languages, id: \.code) { (language: Language) in
+                                Text(language.name)
+                            }
+                        }
+                    }
+                    .onChange(of: lang1) { new in
+                        vm.translate(input: inputText, from: new, to: lang2)
+                        print("Lang1:", new)
+                    }
+                    .onChange(of: lang2) { new in
+                        vm.translate(input: inputText, from: lang1, to: new)
+                        print("Lang2:", new)
+                    }
+
+                    Section {
+                        ZStack(alignment: .topLeading) {
+                            if inputText.isEmpty {
+                                Text("Enter your text here...")
+                                    .foregroundColor(.gray)
+                                    .padding(.horizontal, 8)
+                                    .padding(.vertical, 12)
+                            }
+                            TextEditor(text: $inputText)
+                                .multilineTextAlignment(.leading)
+                                .font(.title2)
+                                .onChange(of: inputText) { _ in
+                                    vm.translate(input: inputText, from: lang1, to: lang2)
+                                }
+                        }
+
+                        if vm.isTranslating {
+                            ProgressView()
+                        } else {
+                            Text(vm.translatedText)
+                                .font(.title2)
+                                .foregroundStyle(.primary)
+                        }
+                    }
+                    .padding(.horizontal)
+                }
+
+                Spacer()
+            }
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background(.gray.opacity(0.2))
+        }
+    }
+}
+
+#Preview {
+    Main()
+}
