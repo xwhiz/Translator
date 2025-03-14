ea540362e4411faa30413ee683f5c8b2ac486423
Hamza <muhammadhamzam1486@gmail.com>
2025-03-14 23:42:09 +0500

    chore: remove the autocomplete code

--- Translator/Extensions/Keyboard+App.swift
+++ Translator/Extensions/Keyboard+App.swift
@@ -28,9 +28,6 @@ extension KeyboardApp {
             // licenseKey: "299B33C6-061C-4285-8189-90525BCAF098",  // Sets up KeyboardKit Pro!
             appGroupId: "group.ai.truid", // Sets up App Group data sync
             locales: .keyboardKitSupported, // Sets up the enabled locales
-            autocomplete: .init( // Sets up custom autocomplete
-                // nextWordPredictionRequest: .claude(apiKey: "")   // Sets up AI-based prediction (add your own key)
-            ),
             deepLinks: .init(
                 app: "translatorapp://" // Defines how to open the app
                 // dictation: "kkdemo://dictation"                  // You can customize any default deep link
