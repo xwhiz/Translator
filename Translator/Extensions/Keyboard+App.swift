c6f070a0d3041941a89d97e680195e9ac6b6ec1c
Hamza <muhammadhamzam1486@gmail.com>
2025-03-14 23:40:20 +0500

    feat: add initial code for the keyboard

---
+++ Translator/Extensions/Keyboard+App.swift
@@ -0,0 +1,40 @@
+//
+//  Keyboard+App.swift
+//  Translator
+//
+//  Created by truID on 14/03/2025.
+//
+
+import KeyboardKit
+
+extension KeyboardApp {
+    /// This `KeyboardApp` value defines the demo app.
+    ///
+    /// The demo uses a `KeyboardKit.license` file to unlock
+    /// KeyboardKit Pro, without having to include a license
+    /// key in the app information below. This also lets the
+    /// app update its license without also having to update
+    /// KeyboardKit version. Note that this file is added to
+    /// both the app and the `KeyboardPro` keyboard.
+    ///
+    /// The App Group ID is only to show you how you can use
+    /// a `KeyboardApp` to set up App Group data syncing for
+    /// an app and its keyboard. It doesn't work in the demo.
+    ///
+    /// See `DemoApp.swift` for more info about the demo app.
+    static var keyboard: KeyboardApp {
+        .init(
+            name: "Translator",
+            // licenseKey: "299B33C6-061C-4285-8189-90525BCAF098",  // Sets up KeyboardKit Pro!
+            appGroupId: "group.ai.truid", // Sets up App Group data sync
+            locales: .keyboardKitSupported, // Sets up the enabled locales
+            autocomplete: .init( // Sets up custom autocomplete
+                // nextWordPredictionRequest: .claude(apiKey: "")   // Sets up AI-based prediction (add your own key)
+            ),
+            deepLinks: .init(
+                app: "translatorapp://" // Defines how to open the app
+                // dictation: "kkdemo://dictation"                  // You can customize any default deep link
+            )
+        )
+    }
+}
